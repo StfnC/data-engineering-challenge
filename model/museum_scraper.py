3cc3d1a13a82b9cddac9e73d5afad19fd1b47fa2
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2023-03-14 17:27:50 -0400

    Added logic to extract country code while scraping

--- model/museum_scraper.py
+++ model/museum_scraper.py
@@ -2,22 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 from bs4.element import ResultSet
 from pandas import DataFrame
+from typing import Callable
 
-
+# FIXME: Add error handling
 # Inspired by: https://www.freecodecamp.org/news/scraping-wikipedia-articles-with-python/
 class MuseumScraper:
-    MUSEUMS_URL = "https://en.wikipedia.org/wiki/List_of_most-visited_museums"
+    WIKI_BASE_URL = "https://en.wikipedia.org"
+    MUSEUMS_PATH = "/wiki/List_of_most-visited_museums"
 
     def __init__(self) -> None:
-        self.__get_page_data()
+        self.__country_codes = {}
 
-    def __get_page_data(self):
-        response = requests.get(url=MuseumScraper.MUSEUMS_URL)
+        self.__get_museum_page_data()
+
+    def __get_museum_page_data(self) -> None:
+        response = requests.get(
+            url=MuseumScraper.WIKI_BASE_URL + MuseumScraper.MUSEUMS_PATH)
 
         self.soup: BeautifulSoup = BeautifulSoup(
             response.content, "html.parser")
 
-    def create_museums_list(self, year: int = 2019):
+    def create_museums_list(self, year: int = 2019) -> DataFrame:
         table = self.__find_year_table(year)
 
         clean_table_data = []
@@ -41,7 +46,11 @@ class MuseumScraper:
 
         location_table_data = museum_name_table_data.next_sibling.next_sibling
 
-        row_data["country"] = location_table_data.span.a["title"]
+        country_name = location_table_data.span.a["title"]
+
+        row_data["country_name"] = country_name
+
+        row_data["country"] = self.__get_country_code(country_name)
 
         row_data["city"] = location_table_data.find_all("a")[-1].text
 
@@ -54,6 +63,25 @@ class MuseumScraper:
         row_data["visits"] = visits
 
         return row_data
+
+    def __get_country_code(self, country: str) -> str:
+        if country in self.__country_codes:
+            return self.__country_codes[country]
+
+        country_slug = country.replace(" ", "_")
+
+        response = requests.get(
+            url=f"{MuseumScraper.WIKI_BASE_URL}/wiki/{country_slug}")
+
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        string_contains_iso_norm: Callable[[str], bool] = lambda s: s and s.startswith("ISO 3166-2:")
+
+        country_code = soup.find(name="a", attrs={"title": string_contains_iso_norm}).text.lower()
+
+        self.__country_codes[country] = country_code
+
+        return self.__country_codes[country]
 
     # FIXME: Add comment explaining the way the page is structured
     def __find_year_table(self, year: int = 2019) -> ResultSet:
